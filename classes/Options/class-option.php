be7259789dad12add4983113b0200557d139bbdc
Pablo Pacheco <pablo.sg.pacheco@gmail.com>
2021-02-20 17:56:10 -0300

    Update filter

--- classes/Options/class-option.php
+++ classes/Options/class-option.php
@@ -89,7 +89,7 @@ if ( ! class_exists( 'FROU\Options\Option' ) ) {
 		 * @since   2.0.0
 		 */
 		public function add_fields( $fields, $section ) {
-			$this->fields = apply_filters( 'frou_get_settings_' . $this->option_id . '_' . $section, $fields, $section );
+			$this->fields = apply_filters( 'frou_get_settings_' . $section, $fields, $section );
 			return $fields;
 		}
 	}
