d52272709522c113cb568d437693be2d3cf7680c
Pablo Pacheco <pablo.sg.pacheco@gmail.com>
2021-02-21 00:10:00 -0300

    Improve text

--- classes/Options/General/class-permalink-update-option.php
+++ classes/Options/General/class-permalink-update-option.php
@@ -129,7 +129,7 @@ if ( ! class_exists( 'FROU\Options\General\Permalink_Update_Option' ) ) {
 				array(
 					'name'    => $this->option_id,
 					'label'   => __( 'Update permalink', 'file-renaming-on-upload' ),
-					'desc'    => __( 'Update permalinks from new added attachment based on the filename', 'file-renaming-on-upload' ),
+					'desc'    => __( 'Update permalinks from new added attachment based on the filename structure', 'file-renaming-on-upload' ),
 					'default' => 'off',
 					'type'    => 'checkbox',
 				),
