c878f14ee07318fca5386b6b701e6428f036ee12
Pablo Pacheco <pablo.sg.pacheco@gmail.com>
2025-01-24 13:32:13 -0300

    Fix post utils

--- classes/class-post-utils.php
+++ classes/class-post-utils.php
@@ -18,31 +18,23 @@ if ( ! defined( 'ABSPATH' ) ) {
 if ( ! class_exists( 'FROU\Post_Utils' ) ) {
 	class Post_Utils {
 
-		//protected $current_media_post;
-
+		/**
+		 * $current_media_post.
+		 *
+		 * @since 2.6.1
+		 *
+		 * @var null
+		 */
 		protected $current_media_post = null;
 
+		/**
+		 * $current_media_post_id.
+		 *
+		 * @since 2.6.1
+		 *
+		 * @var null
+		 */
 		protected $current_media_post_id = null;
-
-		/**
-		 * WP_Post.
-		 *
-		 * @version 2.6.1
-		 * @since   2.6.1
-		 *
-		 * @var \WP_Post
-		 */
-		//protected $media_wp_post;
-
-		/**
-		 * $query_string_post_id/
-		 *
-		 * @version 2.6.1
-		 * @since   2.6.1
-		 *
-		 * @var int
-		 */
-		//protected $query_string_media_post_id;
 
 		/**
 		 * Initializes the class.
@@ -57,13 +49,6 @@ if ( ! class_exists( 'FROU\Post_Utils' ) ) {
 			// PlUpload.
 			add_filter( 'plupload_default_settings', array( $this, 'add_current_media_post_id_to_plupload' ) );
 			add_filter( 'plupload_default_params', array( $this, 'add_current_media_post_id_to_plupload' ) );
-
-			/*add_action( 'init', array( $this, 'store_query_string_media_post_id' ) );
-			add_action( 'init', array( $this, 'store_global_media_wp_post' ) );
-
-			// PlUpload.
-			add_filter( 'plupload_default_settings', array( $this, 'add_media_post_id_to_plupload' ) );
-			add_filter( 'plupload_default_params', array( $this, 'add_media_post_id_to_plupload' ) );*/
 		}
 
 		/**
@@ -105,7 +90,7 @@ if ( ! class_exists( 'FROU\Post_Utils' ) ) {
 				}
 			}
 
-			return $this->current_media_post_id;
+			return apply_filters( 'frou_current_media_post_id', $this->current_media_post_id );
 		}
 
 		/**
@@ -168,16 +153,17 @@ if ( ! class_exists( 'FROU\Post_Utils' ) ) {
 				$the_post = $this->get_current_media_post();
 			}
 			$post_title = '';
+			$info = array();
 			if ( ! empty( $the_post ) && is_a( $the_post, 'WP_Post' ) ) {
 				$url_decoded        = urldecode( $the_post->post_title );
 				$new_post_name      = remove_accents( $url_decoded );
 				$new_post_name      = preg_replace( "/[^a-zA-Z0-9-_.\s]/", "", $new_post_name );
 				$post_title         = sanitize_title( $new_post_name );
-				//$info['raw_string'] = $the_post->post_title;
-				//$info['object']     = $the_post;
+				$info['raw_string'] = $the_post->post_title;
+				$info['object']     = $the_post;
 			}
 
-			return apply_filters( 'frou_get_media_post_title', $post_title );
+			return apply_filters( 'frou_get_media_post_title', $post_title, $info );
 		}
 
 		/**
@@ -200,105 +186,5 @@ if ( ! class_exists( 'FROU\Post_Utils' ) ) {
 
 			return filter_var( $post_id, FILTER_SANITIZE_NUMBER_INT );
 		}
-
-		/**
-		 * add_media_post_id_to_plupload.
-		 *
-		 * @version 2.6.1
-		 * @since   2.6.1
-		 *
-		 * @param $settings
-		 *
-		 * @return mixed
-		 */
-		/*function add_media_post_id_to_plupload( $settings ) {
-			$post_id = $this->get_post_id_from_query_string();
-			if ( ! empty( $post_id ) ) {
-				$settings['frou_query_string_post_id'] = $post_id;
-			}
-
-			return $settings;
-		}*/
-
-		/**
-		 * store_query_string_media_post_id.
-		 *
-		 * @version 2.6.1
-		 * @since   2.6.1
-		 *
-		 * @return void
-		 */
-		/*function store_query_string_media_post_id() {
-			$this->query_string_media_post_id = $this->get_post_id_from_query_string();
-		}*/
-
-		/**
-		 * store_global_media_wp_post.
-		 *
-		 * @version 2.6.1
-		 * @since   2.6.1
-		 *
-		 * @return void
-		 */
-		/*function store_global_media_wp_post() {
-			$this->media_wp_post = $this->get_global_media_wp_post();
-		}*/
-
-		/**
-		 * Returns a post object from the query string. If there isn't, gets it from global $post object.
-		 *
-		 * @version 2.6.1
-		 * @since   2.6.1
-		 *
-		 * @return array|\WP_Post|null
-		 */
-		/*function get_media_wp_post_smart() {
-			$post_id = $this->get_post_id_from_query_string();
-			if ( ! empty( $post_id ) && is_a( $post = get_post( $post_id ), 'WP_Post' ) ) {
-				return $post;
-			} else {
-				return $this->get_global_media_wp_post();
-			}
-		}*/
-
-		/**
-		 * Get gloam media wp post.
-		 *
-		 * @version 2.6.1
-		 * @since   2.6.1
-		 *
-		 * @return array|\WP_Post|null
-		 */
-		/*function get_global_media_wp_post() {
-			if ( ! empty( $this->media_wp_post ) ) {
-				return $this->media_wp_post;
-			}
-			global $post;
-
-			return $post;
-		}*/
-
-		/**
-		 * get_post_id_from_query_string.
-		 *
-		 * @version 2.6.1
-		 * @since   2.6.1
-		 *
-		 * @return int
-		 */
-		/*function get_post_id_from_query_string() {
-			if ( ! empty( $this->query_string_media_post_id ) ) {
-				return $this->query_string_media_post_id;
-			}
-			$post_id_possibilities = apply_filters( 'frou_post_id_query_string_params', array( 'post_id', 'post_ID', 'post', 'product_id', 'frou_query_string_post_id' ) );
-			$post_id               = null;
-			foreach ( $post_id_possibilities as $key ) {
-				if ( isset( $_REQUEST[ $key ] ) ) {
-					$post_id = $_REQUEST[ $key ];
-					break;
-				}
-			}
-			return filter_var( $post_id, FILTER_SANITIZE_NUMBER_INT );
-		}*/
 	}
 }
